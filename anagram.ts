fbd9be2a15f3afb4577079a10c25dee7611c6194
Rupesh Pathak <rupeshpathak@Rupeshs-MacBook-Air.local>
2025-10-22 16:56:43 +0530

    question 242

---
+++ anagram.ts
@@ -0,0 +1,7 @@
+//Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+function isAnagram(s: string, t: string): boolean {
+    let svalue = s.split("").sort();
+    let tvalue = t.split("").sort();
+     return svalue.join("") === tvalue.join("")
+    };
